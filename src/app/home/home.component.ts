e24c4a69664e38b6a2363e85d7de1c4f1b44dc2f
Damian Lee <dlee18@mail.fresnostate.edu>
2019-12-10 00:05:34 -0800

    removed console.logs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,6 @@ import { ToastService } from '../toast/toast.service';
 })
 export class HomeComponent implements OnInit {
 
-  // teams: Array<Tourney> = [];
   teams = [];
   value = '';
   tourneyName = '';
@@ -20,38 +19,26 @@ export class HomeComponent implements OnInit {
   disableSubmitButton = false;
   textDisabled = false;
   inText: string;
+
   constructor(private router: Router, private toastService: ToastService) { }
 
   ngOnInit() {
+
   }
-
-  // navigateTo(path: string) {
-  //   this.router.navigate([path]);
-  // }
-
 
   tourney(newTeam= '') {
     if (this.tourneyName === '' && newTeam === '') {
-      this.toastService.showToast('danger', 'Please enter name of tournament and participants.', 3000);
-      console.log('from if...tourneyName...', this.tourneyName);
-      console.log('from if...newTeam', newTeam);
+      this.toastService.showToast('danger', 'Please enter name of tournament and participants.', 10000);
     } else if (this.tourneyName === '') {
-      this.toastService.showToast('danger', 'Please enter name of tournament', 3000);
-      console.log('from else if1...tourneyName...', this.tourneyName);
-      console.log('from else if1...newTeam', newTeam);
+      this.toastService.showToast('danger', 'Please enter name of tournament', 10000);
     } else if (newTeam === '') {
-      this.toastService.showToast('danger', 'Please enter participants', 3000);
-      console.log('from else if1...tourneyName...', this.tourneyName);
-      console.log('from else if1...newTeam', newTeam);
+      this.toastService.showToast('danger', 'Please enter participants', 10000);
     } else {
       this.tourneyTitle = this.tourneyName;
       this.disableSubmitButton = true;
       this.textDisabled = true;
       this.teams.push(newTeam);
-      console.log('newTeam... ', newTeam);
       const teams = newTeam.split(/[\r\n]+/);
-      console.log(teams);
-      console.log('from else...tourneyName...', this.tourneyName);
     }
   }
 
@@ -60,13 +47,9 @@ export class HomeComponent implements OnInit {
     this.disableSubmitButton = false;
     this.textDisabled = false;
     this.teams = [];
-    // this.inText = '';
-    console.log('Clear all array', this.teams);
-    // only clears textbox on the first click
   }
 
   editSeeds() {
-    // edit team names
     this.disableSubmitButton = false;
     this.textDisabled = false;
   }
